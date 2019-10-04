ebc88837b78148e36f9dfae393bb4f5dad424e6f
Arrebola16 <55503313+Arrebola16@users.noreply.github.com>
2019-10-04 13:12:48 +0200

    Add files via upload

---
+++ N3P117comments.php
@@ -0,0 +1,17 @@
+<?php
+echo("Hacer cosas con los date\n");
+echo("Hacer links \n");
+echo("preguntar si quiero guardar datos en cookies\n");
+echo("Hacer cookies\n");
+echo("cambiar el color de un texto\n");
+echo("cambiar la fuente de un texto\n");
+echo ("Mejorar en php\n");
+echo ("cambiar el tamaño del texto\n");
+echo ("hacer contadores\n");
+echo ("utilizar contadores \n");
+echo ("la explicacion le pongo un 7\n");
+echo("Como estudiante me pondria un 7\n ");
+
+
+
+    ?>
